5ef282c5b6e53d0b512787679468eb63d33473b4
cheretbe <a.a.orlov@gmail.com>
2016-08-22 08:13:16 +0000

    Add sleep time option, cleanup signal handling code

--- check-status.py
+++ check-status.py
@@ -38,20 +38,27 @@ stderr_handler.setLevel(logging.WARNING)
 logger.addHandler(stderr_handler)
 
 data_file = os.path.expanduser('~/.check-status')
+signals_to_handle = {signal.SIGTERM:'SIGTERM', signal.SIGINT:'SIGINT', signal.SIGHUP:'SIGHUP', signal.SIGQUIT:'SIGQUIT'}
 
 def do_check():
   logger.debug('working')
   #1/0
 
+# Interrupt signal handler. Does nothing, just logs interruption cause and exits.
+# Handles the following signals:
+# SIGTERM: process termintaion, SIGTERM: terminal interruption, SIGHUP: terminal closing, SIGQUIT: process quit with dump
 def signal_handler(signum = None, frame = None):
-  logger.info('Caught signal {0}, exiting'.format(signum))
+  logger.info('Caught signal {0} ({1}), exiting'.format(signum, signals_to_handle[signum]))
   sys.exit(0)
 
 def main():
   exit_code = 0
   try:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False)
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False,
+                        help='Increase logging verbosity')
+    parser.add_argument('-s', '--sleep-time', dest='sleep_time', type=int, default=300,
+			help='Sleep time between checks in seconds (default: %(default)d)')
     options = parser.parse_args()
 
     if options.debug:
@@ -61,15 +68,13 @@ def main():
     logger.debug('Script location: {0}'.format(os.path.realpath(__file__)))
     logger.debug('Data file: {0}'.format(data_file))
 
-    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+    for sig in signals_to_handle:
       signal.signal(sig, signal_handler)
 
-    try:
-      while True:
-        do_check()
-        time.sleep(3)
-    except KeyboardInterrupt:
-      logger.info('Keyboard interrupt, exiting')
+    while True:
+      do_check()
+      time.sleep(options.sleep_time)
+
   except Exception as e:
     logger.exception("Unhandled exception")
     exit_code = 1
